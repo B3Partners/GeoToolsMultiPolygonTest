34e3d2257cdf6c0685d2895119345f5da93be6c4
Roy Braam <braam.roy@gmail.com>
2014-01-28 15:56:37 +0100

    working version with geotools 8.0-m4

--- src/main/java/nl/b3p/geotools/multipolygontest/GeotoolsOracleTest.java
+++ src/main/java/nl/b3p/geotools/multipolygontest/GeotoolsOracleTest.java
@@ -6,13 +6,20 @@
 
 package nl.b3p.geotools.multipolygontest;
 
+import com.vividsolutions.jts.geom.Geometry;
 import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.geotools.data.DataStore;
+import org.geotools.data.FeatureSource;
 import org.geotools.data.oracle.OracleNGDataStoreFactory;
+import org.geotools.factory.CommonFactoryFinder;
+import org.geotools.feature.FeatureCollection;
+import org.geotools.feature.FeatureIterator;
+import org.opengis.feature.simple.SimpleFeature;
+import org.opengis.filter.FilterFactory2;
 
 /**
  *
@@ -39,9 +46,26 @@ public class GeotoolsOracleTest {
         //DataStore ds = DataStoreFinder.getDataStore(p);
         
         DataStore ds = new OracleNGDataStoreFactory().createDataStore(p);
-        
+        log.info("FeatureTypes: ");
         for (String s : ds.getTypeNames()){
-            System.out.println(s);
+            log.info(s);
+        }
+        
+        FilterFactory2 ff = CommonFactoryFinder.getFilterFactory2();
+        FeatureSource fs = ds.getFeatureSource("WEGEN_WEGVAK");
+        //FeatureCollection fc = fs.getFeatures(ff.equal(ff.property("STRAATNAAM"), ff.literal("Bleulandpad"),false));
+        FeatureCollection fc = fs.getFeatures();
+        FeatureIterator<SimpleFeature> fi = fc.features();
+        while(fi.hasNext()){
+            SimpleFeature sf = fi.next();
+            Geometry geom = (Geometry)sf.getDefaultGeometry();
+            StringBuffer sb = new StringBuffer();
+            sb.append("Object id: ").append(sf.getAttribute("OBJ_ID"));
+            sb.append(" straatnaam: ").append(sf.getAttribute("STRAATNAAM"));
+            sb.append(" valid: ").append(geom.isValid());            
+            log.info(sb.toString());
+            //log.info(geom.toString());
+            
         }
     }
 }
